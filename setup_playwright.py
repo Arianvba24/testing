88c29edaf9b19ad6a63e38e21f7834f80b740cc8
Arianvba24 <153762786+Arianvba24@users.noreply.github.com>
2024-09-28 02:37:49 +0200

    Update setup_playwright.py

--- setup_playwright.py
+++ setup_playwright.py
@@ -2,8 +2,8 @@ import subprocess
 import sys
 
 def install_playwright():
-    # Run playwright install command
-    subprocess.run([sys.executable, "-m", "playwright", "install"])
+    # This command installs all required browsers (Chromium, Firefox, WebKit)
+    subprocess.run([sys.executable, "-m", "playwright", "install", "--with-deps"])
 
 if __name__ == "__main__":
     install_playwright()
